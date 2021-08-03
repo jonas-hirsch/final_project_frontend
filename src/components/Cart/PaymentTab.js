2b46aeddd22e0c855169eea205f2ca0f53eb9f72
jonas-hirsch <jonasjuanciervos@gmail.com>
2021-08-03 20:32:03 +0200

    fixed the stripe problem

--- src/components/Cart/PaymentTab.js
+++ src/components/Cart/PaymentTab.js
@@ -1,21 +1,17 @@
 import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import MyCheckoutForm from "./MyCheckoutForm.js";
+// import MyCheckoutForm from "./MyCheckoutForm.js";
 import TestCheckoutForm from "./TestCheckoutForm.js";
 
-const promise = loadStripe(
-  "sk_test_51JJwlGH5K3u9KNtaQwTf33XkJkYPNo2I1lqgc38l1yaPL2LGgnIzFdhaX8yyON0xfqvp48tlZpfxXV0fEBQgTkZK006kEjQrS2"
-);
+const promise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
 const PaymentTab = ({ openTab, cartIndex }) => {
   return (
     <div className={openTab === cartIndex + 1 ? "block" : "hidden"} id="link3">
-      <div className="App">
         <Elements stripe={promise}>
           <TestCheckoutForm />
         </Elements>
-      </div>
     </div>
   );
 };
--- src/components/Cart/TestCheckoutForm.js
+++ src/components/Cart/TestCheckoutForm.js
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { useStripe, useElements } from "@stripe/react-stripe-js";
-import {CardElement} from '@stripe/react-stripe-js';
+import { CardElement } from '@stripe/react-stripe-js';
 
 const TestCheckoutForm = () => {
     return (
         <>
-        <CardElement
+        {/* <CardElement
         id="card-element-2"
       //   options={cardStyle}
       />
-      <button>Test</button>
+      <button>Test</button> */}
+      <CardElement
+  options={{
+    style: {
+      base: {
+        fontSize: '16px',
+        color: '#424770',
+        '::placeholder': {
+          color: '#aab7c4',
+        },
+      },
+      invalid: {
+        color: '#9e2146',
+      },
+    },
+  }}
+/>
       </>
     )
 }
