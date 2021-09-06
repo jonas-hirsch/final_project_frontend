1fc8fc637adab5d4d398bdb5dde32ecdbc83d71c
jonas-hirsch <jonasjuanciervos@gmail.com>
2021-09-06 17:53:51 +0200

    HomepageChanges saving

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
         <a href="/#" emailto="http://">
           <span className='flex items-center'>
             <Location24 className='mr-2 text-link' />
-            Some Address
+            Lalastraße 123, München
           </span>
         </a>
       </div>
